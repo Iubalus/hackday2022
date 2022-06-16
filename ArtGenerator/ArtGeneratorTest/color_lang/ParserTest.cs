05459c35b543e5c1b0072ee715f529e4da6d293f
Jubal Rife <2788025+Iubalus@users.noreply.github.com>
2022-06-16 09:47:59 -0600

    Add test

---
+++ ArtGenerator/ArtGeneratorTest/color_lang/ParserTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtGeneratorTest.color_lang
+{
+    internal class ParserTest
+    {
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
